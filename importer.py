8d5bdca23632236e84d5c0babdfcf094e8ad890b
emanegurre <piramideolfativa@gmail.com>
2025-06-15 22:42:38 +0200

    Wrap CLI in main guard

--- importer.py
+++ importer.py
@@ -3,9 +3,11 @@ import argparse
 from pathlib import Path
 import services
 
-parser = argparse.ArgumentParser(description="Importa CSV al sistema.")
-parser.add_argument("csv", type=Path, help="Ruta del CSV de materias")
-args = parser.parse_args()
 
-res = services.import_materials_csv(args.csv)
-print("Importación completada.", res)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Importa CSV al sistema.")
+    parser.add_argument("csv", type=Path, help="Ruta del CSV de materias")
+    args = parser.parse_args()
+
+    res = services.import_materials_csv(args.csv)
+    print("Importación completada.", res)
